49483fe735265d21e42884fb8cd5648e0864091d
Huan <huan@gmail.com>
2017-06-10 17:07:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/shaohuan/coolweather/db/City.java
@@ -0,0 +1,51 @@
+package com.shaohuan.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 创建者： Administrator
+ * <p>
+ * 创建时间： 2017/6/10
+ * <p>
+ * 描述：
+ */
+
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
